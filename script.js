5d819fb15698f989e73532499ea4c7b67a80117f
unknown <rehanmomin796@gmail.com>
2024-08-02 17:57:28 +0530

    updating

--- script.js
+++ script.js
@@ -27,21 +27,21 @@
 // console.log(cats.join("+"));
 
 
-// const companies= [
-//     {name: "Company One", category: "Finance", start: 1981, end: 2004},
-//     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-//     {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-//     {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-//     {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-//     {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-//     {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-//     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-//     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-//   ];
+const companies= [
+    {name: "Company One", category: "Finance", start: 1981, end: 2004},
+    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+  ];
   
-  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+  // const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
   
-  // for(let i = 0; i < ages.length; i++) {
+  // for(let i = 0; i < ages.length; i++  ) {
   //   console.log(ages[i]);
   // }
   
@@ -76,12 +76,13 @@
 
 //   // Filter retail companies
   
-  // const retailCompanies = companies.filter(function(company) {
-  //   if(company.category === 'Retail') {
-  //     return true;
-  //   }
-  // });
-
+  const retailCompanies = companies.filter(function(company) {
+    if(company.category === 'Retail') {
+      return true;
+  
+    }
+  });
+console.log(retailCompanies);
   
   // const retailCompaniess = companies.filter(company => company.category === 'Retail');
   
